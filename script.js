404ab8422e54b55cf8c3687412f58f44e7a49c10
MohcineDev <Mouhcinedev@gmail.com>
2021-04-01 08:59:14 +0100

    clean the code

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const Color = document.querySelector('input[type=color]')
+const inputColor = document.querySelector('input[type=color]')
 const Form = document.querySelector('.loginForm form')
 const Register = document.querySelector('#register')
 const Login = document.querySelector('#login')
 const Submit = document.querySelector('form input[type=submit]')
 const canvas1 = document.getElementById("canvas");
+const toggleScreen = document.querySelector('.screen')
+const haveAccount = document.querySelector('.end p')
 let Input = document.querySelectorAll('form input:not(input[type=submit])')
 
 //************ draw the bottom screen canvas */
@@ -39,7 +41,7 @@ const togglePlaceholder = () => {
 togglePlaceholder()
 
 //********** change the app main color */
-Color.addEventListener('change', function () {
+inputColor.addEventListener('change', function () {
     document.body.style.setProperty('--col', this.value)
 })
 
@@ -48,23 +50,20 @@ Form.addEventListener('submit', (e) => {
     e.preventDefault()
 })
 
-//********** handle form register  */
+//********** handle register form  */
 Register.addEventListener('click', () => {
-    console.log(1111111111);
     Form.children[0].textContent = 'Account Register'
     Form.children[1].setAttribute('placeholder', 'User Name')
     Input = document.querySelectorAll('form input:not(input[type=submit])')
     Submit.setAttribute('value', 'Register')
-    document.querySelector('.end p').textContent = 'You have an account ?'
-    document.querySelector('.screen').classList.toggle('register')
-    // togglePlaceholder()
+    haveAccount.textContent = 'You have an account ?'
+    toggleScreen.classList.toggle('register')
 })
 
-//********** handle form login  */
+//********** handle login form  */
 Login.addEventListener('click', () => {
-    console.log(7777);
     Submit.setAttribute('value', 'Login')
     Form.children[0].textContent = 'Login Account'
-    document.querySelector('.end p').textContent = 'Don\'t have an account ?'
-    document.querySelector('.screen').classList.toggle('register')
+    haveAccount.textContent = 'Don\'t have an account ?'
+    toggleScreen.classList.toggle('register')
 })
